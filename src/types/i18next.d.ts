ab6f5bb36a17f942050ae9778f245089f7531c57
Mario Pérez Hurtado <marioph10@gmail.com>
2023-01-12 09:50:48 +0100

    Added types to i18next

---
+++ src/types/i18next.d.ts
@@ -0,0 +1,10 @@
+import 'i18next'
+import global_en from '../translations/en/global.json'
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    resources: {
+      global: typeof global_en
+    }
+  }
+}
