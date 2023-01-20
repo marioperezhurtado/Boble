bb573568825ac8a4fca4847627b6de7394539efd
Mario Pérez Hurtado <marioph10@gmail.com>
2023-01-20 10:39:52 +0100

    Fixing upload image

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -9,7 +9,7 @@ interface Props {
 const PRIVATE_IMAGE_EXPIRATION = 31536000 // 1 year in seconds
 
 export async function sendImage({ senderId, channelId, image }: Props) {
-  const url = `${channelId}/${Date.now().toString()}}`
+  const url = `${channelId}/${Date.now().toString()}`
 
   const { data: uploadData, error: uploadError } = await supabase.storage
     .from('private-images')
@@ -18,10 +18,10 @@ export async function sendImage({ senderId, channelId, image }: Props) {
 
   const { path } = uploadData
 
-  const { data: urlData } = await supabase.storage
-    .from('images')
+  const { data: urlData, error: urlError } = await supabase.storage
+    .from('private-images')
     .createSignedUrl(path, PRIVATE_IMAGE_EXPIRATION)
-  if (!urlData) throw Error('Failed to create image url')
+  if (urlError) throw Error('Failed to create image url')
 
   const { signedUrl } = urlData
 
